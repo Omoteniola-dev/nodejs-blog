6390d596c3e4d6975036294db4bb9118b91962fb
Teniola <fatunmbiteniola001@gmail.com>
2021-07-14 15:45:00 +0100

    added authROutes and postRoutes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,6 +2,6 @@ const authRoutes = require("./auth.routes");
 const postRoutes = require("./post.routes");
 
 module.exports = (app) => {
-    app.use("/auth", authRoutes);
-    app.use("/post", postRoutes);
+    app.use(authRoutes);
+    app.use(postRoutes);
 };
