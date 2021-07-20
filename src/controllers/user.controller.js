7c11169ac3e16fce6be8da91e30e1b3f0370a2b1
Teniola <fatunmbiteniola001@gmail.com>
2021-07-20 20:55:07 +0100

    implemented registerUser function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,29 @@
 const User = require("../models/user.model");
-const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const { createToken, decodeToken } = require("../services/jwtService");
 
-exports.registerUser = (req, res) => {
-    return null
+exports.registerUser = async(req, res) => {
+    try{
+        const newUser = await User.create({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            username: req.body.username,
+            email: req.body.email,
+            socials: req.body.socials,
+            about: req.body.about
+        });
+        
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        newUser.password = hashedPassword;
+        await newUser.save();
+        
+        let token = createToken(newUser);
+        return res.status(200).json({ message: "New user created", token});
+    }
+    catch(err){
+        throw err;
+    }
 }
 
 exports.loginUser = (req, res) => {
